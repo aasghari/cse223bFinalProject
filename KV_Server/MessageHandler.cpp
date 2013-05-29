db12bbd96a56c5bc58138bf7e2149568dc9f5037
aasghari <aasghari@ucsd.edu>
2013-05-29 01:05:24 -0700

    yep

--- KV_Server/MessageHandler.cpp
+++ KV_Server/MessageHandler.cpp
@@ -12,6 +12,8 @@ MessageHandler::MessageHandler(	const char* multicast_address,const short multic
 		endpoint_(boost::asio::ip::address::from_string(multicast_address), multicast_port), socket_(io_service, endpoint_.protocol()),
 		timer_(io_service), bytesSent(0), bytesRec(0), msgsSent(0), msgsRec(0)
 {
+	socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+	socket_.bind(endpoint_);
     socket_.set_option(boost::asio::ip::multicast::join_group(endpoint_.address()));
     socket_.set_option(boost::asio::ip::multicast::enable_loopback( true ) );
 
@@ -84,7 +86,9 @@ void MessageHandler::asynchWaitForData()
 
 void MessageHandler::startHandler()
 {
+
 	this->asynchWaitForData();
+
 	this->io_service.run();
 }
 void MessageHandler::stopHandler()
--- Makefile
+++ Makefile
@@ -2,11 +2,11 @@ DIRS=common KV_Server
 all:
 	for DIR in $(DIRS);  do \
 	    echo "*****In $$DIR";\
-		$$(cd $$DIR; $(MAKE)) ; \
+		RES=$$(cd $$DIR; $(MAKE)) ; \
 	done
 
 clean:
 	for DIR in $(DIRS);  do \
 	    echo "*****In $$DIR";\
-		$$( cd $$DIR; $(MAKE) clean); \
+		RES=$$( cd $$DIR; $(MAKE) clean); \
 	done
